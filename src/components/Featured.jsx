cb9ec8ff964523c4fa38c0ff05461d290fab5040
Harshal39 <harshalbhute39@gmail.com>
2024-04-13 21:35:19 +0530

    Featured Page

---
+++ src/components/Featured.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className='w-full py-20'>
+      <div className='w-full px-20 border-b-[1px] border-zinc-700 pb-20'>
+        <h1 className='text-7xl font-["Neue_Montreal"] tracking-tight'>Featured projects</h1>
+      </div>
+      <div className='px-20'>
+        <div className="cards w-full flex gap-10 mt-10">
+            
+            <div className="card-container relative w-1/2 h-[75vh]">
+                <h1 className='absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-nonetracking-tighter text-6xl'>{"FYDE".split("").map((item,index)=><span>{item}</span>)}</h1>
+                <div className='card rounded-xl w-full h-full overflow-hidden'>
+                    <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png"></img>
+                </div>
+            </div>
+            <div className="card-container relative w-1/2 h-[75vh]">
+            
+                <div className='card rounded-xl w-full h-full bg-green-600 overflow-hidden'>
+                <h1 className='absolute text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-nonetracking-tighter text-6xl'>{"VISE".split("").map((item,index)=><span>{item}</span>)}</h1>
+                    <img className='w-full h-full bg-cover' src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg"></img>
+                </div>
+            </div>
+            
+        </div>
+      </div>
+    
+    </div>
+  )
+}
+
+export default Featured
