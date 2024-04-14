0eae3219500924ae996cc070447c73c4ece821a0
Harshal39 <harshalbhute39@gmail.com>
2024-04-14 15:52:54 +0530

    final commit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,14 +1,22 @@
+import { motion } from 'framer-motion';
 import React from 'react'
 import { FaArrowUpLong } from "react-icons/fa6";
 
 function LandingPage() {
   return (
-    <div className='w-full h-screen bg-zinc-900 pt-1'>
+    <div data-scroll data-scroll-speed="-.3" className='w-full h-screen bg-zinc-900 pt-1'>
       <div className='textstructure mt-52 px-20'>
         {["We Create", "Eye Opening","Presentations"].map((item,index) => {
             return <div className='masker'>
                 <div className='w-fit flex items-center overflow-hidden'>
-                    {index === 1 && (<div className='w-[8vw] rounded-md h-[4.5vw] relative top-[0.8vw] mr-[1vw] bg-green-500'></div>)}
+                    {index === 1 && 
+                    (<motion.div 
+                    initial={{width: 0}} 
+                    animate={{width:"8vw"}} 
+                    transition={{ease:[0.76,0,0.24,1],duration:1}}
+                    className='w-[8vw] rounded-md h-[4.5vw] relative top-[0.8vw] mr-[0.2vw] bg-green-500'>
+
+                    </motion.div>)}
             <h1 className="uppercase pt-[0.5vw] -mb-[0.5vw] text-[6vw] leading-[6vw] tracking-tighter font-medium">{item}</h1>
         </div>
         </div>
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,11 +3,18 @@ import React from 'react';
 
 function Marquee() {
   return (
-    <div className='w-full py-20 rounded-t-3xl bg-[#004D43]'>
-        <div className='text border-t-2 border-b-2 border-zinc-300 gap-10 flex whitespace-nowrap overflow-hidden'>
-            <h1 className='text-[12vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase pt-1 -mb-[0vw] font-semibold'>we are ochi</h1>
-            <h1 className='text-[12vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase pt-1 -mb-[0vw] font-semibold'>we are ochi</h1>
-           
+    <div data-scroll data-scroll-section data-scroll-speed=".1" className='w-full py-20 rounded-t-3xl bg-[#004D43]'>
+        <div className='text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden'>
+            <motion.h1 
+            initial={{x: 0}} 
+            animate={{x:'-100%'}} 
+            transition={{repeat: Infinity, ease: 'linear', duration: 5}}
+            className='text-[12vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase pt-1 font-semibold pr-20'>we are ochi</motion.h1>
+            <motion.h1 
+            initial={{x: 0}} 
+            animate={{x:'-100%'}} 
+            transition={{repeat: Infinity, ease: 'linear', duration: 5}}
+            className='text-[12vw] leading-none font-["Founders_Grotesk_X-Condensed"] uppercase pt-1  font-semibold pr-20'>we are ochi</motion.h1>
         </div>
       
     </div>
